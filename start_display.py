0db964b2a1fe8326c7f2766ce5a67c8c43690793
Sergio <sergiotz97@gmail.com>
2023-06-07 19:43:24 +0200

    Corrección basic_display

--- start_display.py
+++ start_display.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from piece import Piece
 from player import Player
+from color import Color
 
 @dataclass
 class StartDisplay:
@@ -11,6 +12,6 @@ class StartDisplay:
         self.pieces = pieces
 
 basic_display = StartDisplay(pieces=tuple(
-    [Piece(player=1, color='red')  for _ in range(4)] + 
-    [Piece(player=1, color='blue') for _ in range(4)]
+    [Piece(player=Player(1), color=Color('red'))  for _ in range(4)] + 
+    [Piece(player=Player(1), color=Color('blue')) for _ in range(4)]
 ))
