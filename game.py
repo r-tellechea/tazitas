80e6e0655be2947cc8b4cf33222ddd4642477f37
Sergio <sergiotz97@gmail.com>
2023-06-07 21:13:55 +0200

    Game TODO

---
+++ game.py
@@ -0,0 +1,7 @@
+from board import Board
+from start_display import StartDisplay
+
+class Game:
+    #TODO
+    def __init__(self, player_0_start_display : StartDisplay, player_1_start_display : StartDisplay) -> None:
+        pass
