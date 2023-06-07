d0a5a3de255049a50a4bdafaa5941400ccb01b33
Sergio <sergiotz97@gmail.com>
2023-06-07 21:11:48 +0200

    TODO movimientos

--- board.py
+++ board.py
@@ -32,12 +32,40 @@ class Board:
 	def get_piece(self, coords : Coords) -> Piece:
 		return self.board[coords.row][coords.col]
 	
-	def set_piece(self, coords : Coords, piece : Piece) -> None:
+	def set_piece(self, coords : Coords, piece : Piece=None) -> None:
 		self.board[coords.row][coords.col] = piece
+	
+	def is_piece(self, coords : Coords) -> bool:
+		return isinstance(self.get_piece(coords), Piece)
+	
+	def is_piece_of_player(self, coords : Coords, player : Player) -> bool:
+		if not self.is_piece(coords):
+			raise ValueError('There is not piece in the selected cell.')
+		return self.get_piece(coords).player == player
 
 	def insert_start_display (self, player : Player, start_display : StartDisplay) -> None:
 		for coords, piece in zip(start_display_coords(player), start_display.pieces):
 			self.set_piece(coords=coords, piece=piece)
+		
+	def check_victory(self) -> Player:
+		#TODO
+		# Devuelve None si la partida sigue o un jugador si éste gana.
+		pass
+
+	def move_piece(self, start_coords : Coords, end_coords : Coords):
+		#TODO
+		# Comprobar:
+		# - Las coordenadas de origen corresponden a una pieza.
+		# - Las coordenadas de destino está en la lista de casillas disponibles para la pieza.
+		# - En caso de que haya una pieza en el destino no puede ser del mismo jugador.
+		# - Cosas de las esquinas
+
+		# Copiar pieza en la casilla de destino.
+		# Poner vacía la casilla de origen.
+
+		# Comprobar victorias.
+
+		pass
 
 	def df(self, player : Player=None) -> pd.DataFrame:
 		return board_to_dataframe(board=self, player=player)
